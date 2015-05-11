e21cf8a0bee3dafd3031f8cb0a9d254e3fd9a70a
Jeff Patzer <jpatzer@gmail.com>
2015-05-10 23:58:29 -0700

    update git

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'jpatzer:onfleet',
+  name: 'jeffpatzer:onfleet',
   version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: 'OnFleet API for Meteor',
