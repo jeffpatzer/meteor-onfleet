b9a8d97e26e5ceb7b018c1ff152c1c76ed0508d4
Jeff Patzer <jeff.patzer@moovweb.com>
2015-05-11 11:56:44 -0700

    bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jeffpatzer:onfleet',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'OnFleet API for Meteor',
   // URL to the Git repository containing the source code for this package.
